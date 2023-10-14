3db8fe94e3f1194424b2a492b64b146cc4caaf38
thumeka <thumekatembani21@gmail.com>
2023-10-14 04:31:40 -0700

    Task 6 test

--- console.py
+++ console.py
@@ -14,6 +14,15 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """Defines the AirBnB command interpreter"""
     prompt = "(hbnb) "
+    classses = {
+            "Amenity": Amenity,
+            "BaseModel": BaseModel,
+            "City": City,
+            "Place": Place,
+            "Review": Review,
+            "State": State,
+            "User": User
+        }
 
     def precmd(self, arg):
         """Parses command input"""
@@ -32,9 +41,24 @@ class HBNBCommand(cmd.Cmd):
         print()
         return True
 
+    def check_class(self, class_name):
+        """checks if clss_name is valid"""
+        if class_name is not self.classes:
+            print("** class doesnt exist **")
+            return False
+        return True
+
     def do_create(self, arg):
         """Creates a new instance of a given class"""
-        pass
+
+        """checks the classes"""
+        if not self.check_class(arg):
+            return
+
+        """Saves the instance"""
+        instance = HBNBCommand.classes[arg]()
+        instance.save()
+        print(instance.id)
 
     def do_show(self, arg):
         """Prints the string representation of an instances"""
